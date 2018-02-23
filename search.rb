cc797ba5a7a34bdf03076dae3ab2ea83daf9e9b6
ben <bennappier@gmail.com>
2018-02-22 18:34:22 -0800

    Add basic search tests with marshal dump to avoid api hits

--- search.rb
+++ search.rb
@@ -5,3 +5,27 @@ require 'pry'
 GOOGLE_API_KEY = ENV['GOOGLE_API_KEY']
 GOOGLE_SEARCH_CX = ENV['GOOGLE_SEARCH_CX']
 
+def search_for(question, answer, stubbed_result = nil)
+  if stubbed_result
+    result = stubbed_result
+  else
+    result = GoogleCustomSearchApi.search("#{question} #{answer}")
+  end
+
+  {
+    answer: answer,
+    result: score(result)
+  }
+end
+
+def search_and_parse_response(question_and_answers)
+  question = question_and_answers[:question]
+  answers = question_and_answers[:answers]
+
+  answers.map { |answer| search_for(question, answer) }
+end
+
+# Not a very good score
+def score(result)
+  result['searchInformation']['totalResults']
+end
--- test.rb
+++ test.rb
@@ -40,10 +40,45 @@ def run_ocr_and_parse_tests
   test_output(actual, expectation)
 end
 
-def run_search_tests
-  # TODO
+def serialize_search_result
+  answers_and_question = {
+    question: "In 1954, Texas Instruments helped create the ﬁrst commercial version of what?",
+    answers: ["Calculator", "Walkie-talkie", "Transistor radio"]
+  }
+
+  answers_and_question[:answers].each do |answer|
+    result = GoogleCustomSearchApi.search("#{answers_and_question[:question]} #{answer}")
+
+    File.open("search-#{answer}.txt", "wb") do |file|
+      Marshal.dump(result, file)
+    end
+  end
 end
 
-# run_ocr_and_parse_tests
-run_search_tests
+def test_stubbed_results
+  answers_and_question = {
+    question: "In 1954, Texas Instruments helped create the ﬁrst commercial version of what?",
+    answers: ["Calculator", "Walkie-talkie", "Transistor radio"]
+  }
 
+  results = answers_and_question[:answers].map do |answer|
+    result = nil
+    File.open("search-#{answer}.txt","rb") { |f| result = Marshal.load(f) }
+    result
+  end
+
+  scores = []
+  answers_and_question[:answers].each_with_index do |answer, i|
+    scores << search_for(answers_and_question[:question], answer, results[i])
+  end
+
+  expected_scores = [
+    {:answer=>"Calculator", :result=>"38800"}, # Note this is the wrong answer, need to fix score
+    {:answer=>"Walkie-talkie", :result=>"120"},
+    {:answer=>"Transistor radio", :result=>"1030"}
+  ]
+  test_output(scores, expected_scores)
+end
+
+run_ocr_and_parse_tests
+test_stubbed_results
